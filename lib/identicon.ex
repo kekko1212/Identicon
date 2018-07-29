8fa67eb7b971d8f1ea5a8bb3ac0e1ad71dcc43cd
Francesco Lo Franco <francesco@flypay.co.uk>
2018-07-29 18:32:13 +0100

    Implement functionality to generate image and save them locally

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,72 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_grid
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
 
-  @doc """
-  Hello world.
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
 
-  ## Examples
+    %Identicon.Image{hex: hex}
+  end
 
-      iex> Identicon.hello()
-      :world
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
-  """
-  def hello do
-    :world
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+      [col1, col2 | _tails] = row
+      row ++ [col2, col1]
+  end
+
+  def filter_grid(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+      pixel_map = Enum.map grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end
+
+      %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+
+    Enum.each pixel_map, fn {top_left, bottom_right} ->
+      :egd.filledRectangle(image, top_left, bottom_right, :egd.color(color))
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image_file, input) do
+    File.write("#{input}.png", image_file)
   end
 end
